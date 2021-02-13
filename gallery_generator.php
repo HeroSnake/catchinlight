34683aaefd8051baff07b2212e4e2e0f451107c7
Florent SYX <florent.syx@outlook.fr>
2021-02-13 02:49:54 +0100

    fixes gallery

--- gallery_generator.php
+++ gallery_generator.php
@@ -4,23 +4,28 @@ $id_image; $likes;
 $sth = $bdd->prepare("SELECT * FROM image WHERE gallery_id = $id_gallery ORDER BY date_add DESC");
 $sth->execute();
 $images = $sth->fetchAll(\PDO::FETCH_ASSOC);
+$sth = $bdd->prepare("SELECT nom_gallery FROM galleries WHERE id = $id_gallery");
+$sth->execute();
+$gallery_name = $sth->fetchColumn();
 ?>
 <section id="photos">
     <?php
     foreach($images as $image){
-        $name = $image["id"] . "." . $image["extension"];
+        $link = "img/$gallery_name/".$image["id"] . "." . $image["extension"];
         $liked = false;
         if(isset($_COOKIE[$image["id"]])){
             $liked = true;
         }?>
-        <a class="change-icon" id="<?=$image["id"]?>" name="<?=$id_gallery?>">
-            <i class="far fa-heart fa-lg" <?php if($liked){?> style="display: none;" <?php }?>></i>
-            <i class="fas fa-heart fa-lg pulse text-danger" <?php if(!$liked){?> style="display: none;" <?php }?>></i>
-        </a>
-        <span class="like-text"><?=$image["likes"]?></span>
-        <a class="lightbox" href="img/portraits/<?=$name?>.">
-            <img src="img/portraits/<?=$name?>" alt="img_gallery">
-        </a>
+        <div>
+            <a class="change-icon" id="<?=$image["id"]?>" name="<?=$id_gallery?>">
+                <i class="far fa-heart fa-lg" <?php if($liked){?> style="display: none;" <?php }?>></i>
+                <i class="fas fa-heart fa-lg pulse text-danger" <?php if(!$liked){?> style="display: none;" <?php }?>></i>
+            </a>
+            <span class="like-text"><?=$image["likes"]?></span>
+            <a class="lightbox" href="<?=$link?>">
+                <img src="<?=$link?>" alt="img_gallery">
+            </a>
+        </div>
     <?php } ?>
 </section>
 <?php require_once "endPage.php";
