a150f643c96bbe9ff906451314fea6abfd787cdf
Florent SYX <florent.syx@outlook.fr>
2021-02-13 14:05:01 +0100

    removing celebrities

--- celebrites.php
+++
@@ -1,6 +0,0 @@
-<?php
-$id_gallery = 3;
-$titre_page = "Célébrités";
-$meta_description = "Gallerie d'images";
-$css_gallery = '<link rel="stylesheet" href="css/grid_img_3.css">';
-require "gallery_generator.php";
