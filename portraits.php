d733f920e8e6c0be15b1742ef82f4b3fbfb065b4
Florent SYX <florent.syx@outlook.fr>
2021-02-13 13:47:24 +0100

    remove only one loading for gallery

--- portraits.php
+++ portraits.php
@@ -3,5 +3,5 @@ $titre_page = "Portraits";
 $meta_description = "Gallerie d'images";
 $id_gallery = 1;
 $css_gallery = '<link rel="stylesheet" href="css/grid_img_5.css">';
-require_once "gallery_generator.php";
+require "gallery_generator.php";
 
